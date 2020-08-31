944247d8930b515268e99a2fc0eea88d5918c0e9
Alexander Sokol <reb2020@gmail.com>
2020-09-01 00:34:04 +0300

    update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,11 +1,9 @@
-declare namespace FiveNoSchema {
-  type AllowTypes = 'string' | 'number' | 'boolean' | 'object' | 'array' | 'date';
+import SchemaCore from '@5no/schema'
 
-  interface List<T> {
-    [name: string]: T;
-  }
+declare module '@5no/schema' {
+  export type AllowTypes = 'string' | 'number' | 'boolean' | 'object' | 'array' | 'date';
 
-  interface InitializeFnParams {
+  export interface InitializeFnParams {
     name: string;
     type: AllowTypes;
     value: any;
@@ -17,7 +15,7 @@ declare namespace FiveNoSchema {
     previousResult: any;
   }
 
-  interface FieldsSchema {
+  export interface FieldsSchema {
     [name: string]: {
       type: string | object;
       defaultValue?: any;
@@ -31,62 +29,14 @@ declare namespace FiveNoSchema {
     }
   }
 
-  type Fn = (props: InitializeFnParams) => any;
-
-  interface Validators {
-    type: Fn;
-    required: Fn;
-    date: Fn;
-    email: Fn;
-    enum: Fn;
-    number: Fn;
-  }
-
-  interface Filters {
-    number: Fn;
-    string: Fn;
-    boolean: Fn;
-    object: Fn;
-    date: Fn;
-    array: Fn;
-    trim: Fn;
-    lowerCase: Fn;
-    upperCase: Fn;
-  }
-
-  type FunctionArguments = Omit<InitializeFnParams, 'options'>;
-
-  interface FnInit {
-    fn: string | Fn;
-    options?: {
-      [name: string]: any;
-    };
-  }
-
-  interface InitializeFunctions {
-    fn: Fn,
-    data: InitializeFnParams;
-  }
-
-  interface Result {
-    field: string;
-    child: boolean;
-    result: any;
-  }
-
-  interface ChildResult {
-    field: string;
-    child: boolean;
-    errors: any;
-    result: any;
-  }
-
   type SchemaFn = (data: object) => Promise<object>;
 
-  interface Schema {
+  export interface Schema {
     isChild: boolean;
     filter: SchemaFn;
     validate: SchemaFn;
     json: () => object;
   }
+
+  export = SchemaCore
 }
